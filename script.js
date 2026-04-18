fec20d6c2a0215bde817d4337e378fa42f1bdc1a
Arijit <arijitroylochan@gmail.com>
2026-04-18 19:40:49 +0530

    Design of the Buttons Changed

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 console.log("Lets write JavaScript")
+
+async function getSongs(){
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("songs")[1])
+        }
+    }
+    return songs
+}
+
+async function main(){
+    //get list of all the songs
+    let songs = await getSongs()
+    console.log(songs)
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>
+                            <img class="invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replace("%5C"," ").replace(".mp3","")}</div>
+                                <div>Arijit</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="play.svg" alt="">
+                            </div>
+                        </li>`
+    }
+
+    //play the first song
+    var audio = new Audio(songs[0]);
+    audio.play();
+    audio.addEventListener("loadeddata", ()=>{
+        console.log(audio.duration, audio.currentSrc,audio.currentTime)
+    })
+}
+main()
